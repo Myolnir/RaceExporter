edbb968e9651545affa6cfcd365d7d74ffde9ff6
angel <angel.muelas@denizen.io>
2019-07-31 17:02:15 +0200

    fix: removed sensitive data

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongo: {
-    url: 'mongodb://angel:it6v5AD1@ds123136.mlab.com:23136/race_exporter',
+    url: 'mongodb://shhhh:shhhh@ds123136.mlab.com:23136/race_exporter',
   },
 };
