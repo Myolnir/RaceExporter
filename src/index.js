4c6455fa4a9b9d9a42e1a6ae227ed48427e32c66
angel <angel.muelas@denizen.io>
2019-08-28 12:07:09 +0200

    wip: changed logger to bunyan

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const restify = require('restify');
 const restifyPlugins = require('restify-plugins');
 const route_catalog = require('./routes/index');
 
-// To backup the activityes daily
+// Backing up the activities daily
 cron.schedule(config.strava.backupTime, async () => {
   console.log('Updating Strava Activities');
   await raceExporterService.backupAllActivities(logger);
@@ -16,15 +16,20 @@ cron.schedule(config.strava.backupTime, async () => {
 const server = restify.createServer({
   name: config.name,
   version: config.version,
+  log: logger,
 });
+
 /**
   * Middleware
   */
+// Extend logger using the plugin.
+server.use(restifyPlugins.requestLogger());
 server.use(restifyPlugins.jsonBodyParser({mapParams: true}));
 server.use(restifyPlugins.acceptParser(server.acceptable));
 server.use(restifyPlugins.queryParser({mapParams: true}));
 server.use(restifyPlugins.fullResponse());
 server.listen(config.port, () => {
+  logger.debug('test logger');
   console.log('%s listening at %s', server.name, server.address().port);
   route_catalog(server);
 });
--- src/util/logger.js
+++ src/util/logger.js
@@ -1,32 +1,4 @@
-const winston = require('winston');
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    // Creates new transport to log to the Console info level logs.
-    new winston.transports.Console({
-      level: 'info',
-      timestamp: () => new Date().toString(),
-      json: true,
-    }),
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({filename: 'error.log', level: 'error'}),
-    new winston.transports.File({filename: 'combined.log'}),
-    
-  ],
+const bunyan = require('bunyan');
+module.exports = bunyan.createLogger({
+  name: 'StravaActivitiesApi',
 });
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
-
-module.exports = logger;
